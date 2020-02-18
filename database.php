69b6ea0fd98e31c5d048675a03db96fd4d8fd059
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-02-17 23:13:01 -0600

    first commit

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'ToDoList';
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
