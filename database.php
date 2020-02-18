8fc174c9d2500d59d486167f383aa7fc7ec5aaa9
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-02-17 23:11:51 -0600

    Update database.php

--- database.php
+++ database.php
@@ -1,14 +1,10 @@
 <?php
-    $dsn = 'localhost';
-    $username = 'root';
-    $password = '';
+
+$dbServername = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'ToDoList';
+
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
-    try {
-        $db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('database_error.php');
-        exit();
-    }
 ?>
