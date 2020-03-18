9465973060c536442dfea639de6e2c04b2f19a74
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-18 00:12:49 -0500

    update

--- delete_item.php
+++ delete_item.php
@@ -1,18 +1,18 @@
 <?php
-include_once('database.php');
-include_once('index.php');
+require_once('database.php');
 // Get ID
-$ItemNum = filter_input(INPUT_POST, 'ItemNum', FILTER_VALIDATE_INT);
+$ItemNum = filter_input(INPUT_POST, 'item_num', FILTER_VALIDATE_INT);
 
 // Delete
-if ($ItemNum != false) {
+if ($item_num != false) {
     $query = 'DELETE FROM todoitems
-              WHERE ItemNum = :ItemNum';
+              WHERE ItemNum = :item_num';
     $statement = $db->prepare($query);
-    $statement->bindValue(':ItemNum', $ItemNum);
+    $statement->bindValue(':item_num', $item_num);
     $success = $statement->execute();
     $statement->closeCursor();    
 }
 
 // display items
 include('index.php');
+?>
