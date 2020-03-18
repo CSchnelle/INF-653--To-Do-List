3c03568afe616254e371de855a0c71584060265e
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-18 01:09:04 -0500

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,11 @@
 <?php
 $dsn = 'mysql:host=localhost;dbname=todolist';
-$username = 'root';
+    $username = 'mgs_user';
+    $password = 'pa55word';
+//$username = 'root';
 
 try {
-  $db = new PDO($dsn, $username);
+  $db = new PDO($dsn, $username, $password);
 } catch (PDOException $e) {
   $error_message = $e->getMessage();
   include('database_error.php');
