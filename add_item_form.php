8188b7bd318b8ffc3c05aa8f434f7110324d1926
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-18 02:06:58 -0500

    Update add_item_form.php

--- add_item_form.php
+++ add_item_form.php
@@ -8,24 +8,26 @@
 <!--body section-->
 <body>
      <header>
-         <h1>My To Do List</h1>
-     </header>
-     <main>
-         <h2>Add Item</h2>
-         <form action="add_item.php" method="post" id="add_item_form">
-             <label>Title:</label>
-             <input type="text" name="title" max="20" required><br>
-             
-             <label>Description:</label>
-             <input type="text" name="title" max="20" required><br>
-             
-             <label>&nbsp;</label>
-             <input type="submit" value="Add Item" class="button blue"><br>
-         </form>
-         <p><a href="index.php">View To Do List</a></p>
+        <h1>My To Do List</h1>
+    </header>
+    <main>
+        <h2>Add Item</h2>
+        <form action="add_item.php" method="post" id="add_item_form">
+
+            <label>Title:</label>
+            <input type="text" name="title" max="20" required><br>
+
+            <label>Description:</label>
+            <input type="text" name="description" max="50" required><br>
+
+            <label>&nbsp;</label>
+            <input type="submit" value="Add Item" class="button blue"><br>
+        </form>
+        <p><a href="index.php">View To Do List</a></p>
     </main>
+
     <footer>
-    <p>&copy; <?php echo date("Y");?>My To Do List</p>
+        <p>&copy; <?php echo date("Y"); ?> My To Do List</p>
     </footer>
-    </body>
+</body>
 </html>
